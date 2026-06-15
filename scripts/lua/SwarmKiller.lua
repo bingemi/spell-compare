f8abf6bdd45cdf65b85d66caffd5d5795b1f755c
Bryon Ingemi <bingemi@gmail.com>
2026-06-15 12:51:03 -0400

    update swarmkiller script again.

--- scripts/lua/SwarmKiller.lua
+++ scripts/lua/SwarmKiller.lua
@@ -1,22 +1,32 @@
 local mq = require('mq')
 
 local radius = 30
+local zradius = 20
 
-mq.delay(200)
-mq.cmd('mqtarget npc radius 40 zradius 20')
+print("SwarmKiller started!")
+
+mq.delay(500)
+mq.cmd(('/mqtarget npc radius %d zradius %d'):format(radius, zradius))
 mq.cmd('/assistme')
 
+local lastTargetID = 0
+
 while true do
-    local npcCount = mq.TLO.SpawnCount(('npc radius %d'):format(radius))()
+    local npcCount = mq.TLO.SpawnCount(('npc radius %d zradius %d'):format(radius, zradius))()
 
     if npcCount == 0 then
+        print("Swarm cleared! SwarmKiller exiting.")
         break
     end
 
     local targetID = mq.TLO.Target.ID()
-    if not targetID or targetID == 0 then
+    if targetID and targetID ~= 0 and targetID ~= lastTargetID then
+        mq.delay(100)
+        mq.cmd('/assistme')
+        lastTargetID = targetID
+    elseif not targetID or targetID == 0 then
+        mq.cmd(('/mqtarget npc radius %d zradius %d'):format(radius, zradius))
         mq.delay(200)
-        mq.cmd('mqtarget npc radius 40 zradius 20')
         mq.cmd('/assistme')
     end
 
