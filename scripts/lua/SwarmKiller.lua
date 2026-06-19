184db72e485917e92c8eb86ee31703c59a29d684
chrisrysdyk <32724447+chrisrysdyk@users.noreply.github.com>
2026-06-19 04:25:49 -0400

    Change target command to exclude pets in SwarmKiller

--- scripts/lua/SwarmKiller.lua
+++ scripts/lua/SwarmKiller.lua
@@ -6,13 +6,13 @@ local zradius = 20
 print("SwarmKiller started!")
 
 mq.delay(500)
-mq.cmd(('/mqtarget npc radius %d zradius %d'):format(radius, zradius))
+mq.cmd(('/mqtarget npc nopet radius %d zradius %d'):format(radius, zradius))
 mq.cmd('/assistme')
 
 local lastTargetID = 0
 
 while true do
-    local npcCount = mq.TLO.SpawnCount(('npc radius %d zradius %d'):format(radius, zradius))()
+    local npcCount = mq.TLO.SpawnCount(('npc nopet radius %d zradius %d'):format(radius, zradius))()
 
     if npcCount == 0 then
         print("Swarm cleared! SwarmKiller exiting.")
@@ -26,7 +26,7 @@ while true do
 		mq.cmd('/face fast')
         lastTargetID = targetID
     elseif not targetID or targetID == 0 then
-        mq.cmd(('/mqtarget npc radius %d zradius %d'):format(radius, zradius))
+        mq.cmd(('/mqtarget npc nopet radius %d zradius %d'):format(radius, zradius))
         mq.delay(200)
         mq.cmd('/assistme')
 		mq.cmd('/face fast')
